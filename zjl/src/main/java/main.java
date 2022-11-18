a371d9b0708bdb55ed4bdc28a14e376cbf06695f
zjl <2332717429@qq.com>
2022-11-18 20:04:19 +0800

    second commit

--- zjl/src/main/java/main.java
+++ zjl/src/main/java/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("idea----git");
+        System.out.println("02");
+        System.out.println("idea----git03");
     }
 }
